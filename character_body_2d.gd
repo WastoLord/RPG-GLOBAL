d83bb5e5eb6e66629653f4ddedc238c2ea7f3b9b
Kali Live user <wastolord13@gmail.com>
2025-09-24 20:54:26 +0000

    pulo

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+var is_animating = false
+
 func _process(_delta):
 	if Input.is_action_pressed("ui_left"):
 		global_position.x -=3
@@ -12,13 +14,14 @@ func _process(_delta):
 		
 	elif Input.is_action_pressed("ui_down"):
 		global_position.y +=3
-	if Input.is_action_pressed("ui_accept"):
-		while global_scale.x < 2:
-			global_scale.x += 0.1
-			global_scale.y += 0.1
-			break
-			
-		while global_scale.x > 1:
-			global_scale.x -= 0.1
-			global_scale.y -= 0.1 
-			continue
+		
+	if Input.is_action_pressed("ui_accept") and not is_animating:
+		is_animating = true
+		var tween = create_tween()
+		
+		tween.tween_property(self, "scale", Vector2(1.5, 1.5), 0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+		
+		tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+		
+		await tween.finished
+		is_animating = false
